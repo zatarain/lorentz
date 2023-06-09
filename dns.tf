32ec18c5d77edc9806841c49179744bd9ce529b8
Ulises Tirado Zatarain <ulises.tirado@cimat.mx>
2023-06-09 18:35:53 +0100

    LRNT-005: Fixing DNS delegation sets to sub account using default acoount one
    Signed-off-by: Ulises Tirado Zatarain <ulises.tirado@cimat.mx>

--- dns.tf
+++ dns.tf
@@ -1,3 +1,12 @@
+locals {
+  name_servers = [
+    "ns-11.awsdns-01.com",
+    "ns-1146.awsdns-15.org",
+    "ns-1999.awsdns-57.co.uk",
+    "ns-887.awsdns-46.net",
+  ]
+}
+
 resource "aws_route53_delegation_set" "dns" {
   reference_name = "Delegation Set (${terraform.workspace})"
 }
@@ -7,7 +16,7 @@ resource "aws_route53domains_registered_domain" "realm" {
   domain_name = each.value
 
   dynamic "name_server" {
-    for_each = toset(aws_route53_delegation_set.dns.name_servers)
+    for_each = toset(terraform.workspace == "default" ? aws_route53_delegation_set.dns.name_servers : local.name_servers)
     content {
       name = name_server.value
     }
@@ -17,7 +26,7 @@ resource "aws_route53domains_registered_domain" "realm" {
 resource "aws_route53_zone" "kingdom" {
   for_each          = toset(local.configuration.dns.zones)
   name              = each.value
-  delegation_set_id = aws_route53_delegation_set.dns.id
+  delegation_set_id = terraform.workspace == "default" ? aws_route53_delegation_set.dns.id : null
 }
 
 locals {
