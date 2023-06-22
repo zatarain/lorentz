41c20f5a82e0aa340d017337eb39d850f50192dd
Ulises Tirado Zatarain <ulises.tirado@cimat.mx>
2023-06-22 14:54:24 +0100

    LRNT-006: SSL Certificates for domain names
    Signed-off-by: Ulises Tirado Zatarain <ulises.tirado@cimat.mx>

---
+++ ssl.tf
@@ -0,0 +1,55 @@
+resource "aws_acm_certificate" "kingdom" {
+  for_each          = toset(local.configuration.dns.domains)
+  domain_name       = "*.${each.value}"
+  validation_method = "DNS"
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+locals {
+  ssl_validation_records = flatten([
+    for kingdom, certificate in aws_acm_certificate.kingdom : [
+      for option in certificate.domain_validation_options : {
+        domain  = kingdom
+        name    = option.resource_record_name
+        record  = option.resource_record_value
+        type    = option.resource_record_type
+        zone_id = data.aws_route53_zone.kingdom[kingdom].zone_id
+      }
+    ]
+  ])
+}
+
+data "aws_route53_zone" "kingdom" {
+  for_each = toset(local.configuration.dns.domains)
+  #provider = aws.root
+  name = each.value
+}
+
+resource "aws_route53_record" "ssl" {
+  for_each = toset(local.ssl_validation_records)
+
+  allow_overwrite = true
+  name            = each.value.name
+  records         = [each.value.record]
+  ttl             = 60
+  type            = each.value.type
+  zone_id         = each.value.zone_id
+}
+
+locals {
+  validation_record_fqdns = {
+    for domain in local.configuration.dns.domains : domain => [
+      for record in aws_route53_record.ssl :
+      record.fqdn if record.zone_id == data.aws_route53_zone.kingdom[domain].zone_id
+    ]
+  }
+}
+
+resource "aws_acm_certificate_validation" "ssl" {
+  for_each                = toset(local.configuration.dns.domains)
+  certificate_arn         = aws_acm_certificate.kingdom[each.value].arn
+  validation_record_fqdns = local.validation_record_fqdns[each.value]
+}
