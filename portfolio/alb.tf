9ec103ea6cf8b9eee86449a74268e4dab6616d5f
Ulises Tirado Zatarain <ulises.tirado@cimat.mx>
2023-06-23 20:45:00 +0100

    LRNT-009: Removing old security groups for the application
    Signed-off-by: Ulises Tirado Zatarain <ulises.tirado@cimat.mx>

--- portfolio/alb.tf
+++ portfolio/alb.tf
@@ -10,30 +10,6 @@ resource "aws_alb" "back-end" {
   ]
 }
 
-# Creating a security group for the load balancer:
-resource "aws_security_group" "back-end-entry-point" {
-  ingress {
-    from_port   = 80 # Allowing traffic in from port 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"] # Allowing traffic in from all sources
-  }
-
-  ingress {
-    from_port   = 443 # Allowing traffic in from port 443
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"] # Allowing traffic in from all sources
-  }
-
-  egress {
-    from_port   = 0             # Allowing any incoming port
-    to_port     = 0             # Allowing any outgoing port
-    protocol    = "-1"          # Allowing any outgoing protocol
-    cidr_blocks = ["0.0.0.0/0"] # Allowing traffic out to all IP addresses
-  }
-}
-
 resource "aws_lb_target_group" "back-end-workers" {
   name        = "${var.prefix}-back-end-workers"
   port        = 80
@@ -83,30 +59,6 @@ resource "aws_alb" "front-end" {
   security_groups = [
     aws_security_group.entry-point.id,
   ]
-}
-
-# Creating a security group for the load balancer:
-resource "aws_security_group" "front-end-entry-point" {
-  ingress {
-    from_port   = 80 # Allowing traffic in from port 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"] # Allowing traffic in from all sources
-  }
-
-  ingress {
-    from_port   = 443 # Allowing traffic in from port 80
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"] # Allowing traffic in from all sources
-  }
-
-  egress {
-    from_port   = 0             # Allowing any incoming port
-    to_port     = 0             # Allowing any outgoing port
-    protocol    = "-1"          # Allowing any outgoing protocol
-    cidr_blocks = ["0.0.0.0/0"] # Allowing traffic out to all IP addresses
-  }
 }
 
 resource "aws_lb_target_group" "front-end-workers" {
