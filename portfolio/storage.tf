aa8656ba5f74c3db13455b8765d46ecb3626147d
Ulises Tirado Zatarain <ulises.tirado@cimat.mx>
2023-07-07 06:47:31 +0100

    LRNT-014: Adding PostgreSQL instance in Amazon RDS
    Signed-off-by: Ulises Tirado Zatarain <ulises.tirado@cimat.mx>

--- portfolio/storage.tf
+++ portfolio/storage.tf
@@ -6,3 +6,18 @@ resource aws_s3_bucket "cv-storage" {
 		Environment = terraform.workspace
 	}
 }
+
+resource "aws_db_instance" "postgres" {
+  allocated_storage           = 20
+	max_allocated_storage				= 100
+  identifier                  = "${terraform.workspace}"
+	db_name											= "geofootball"
+  engine                      = "postgresql"
+  engine_version              = "14.7"
+  instance_class              = "db.t3.micro"
+  manage_master_user_password = true
+	multi_az  									= false
+  username                    = "postgres"
+  parameter_group_name        = "default.postgres14"
+	publicly_accessible					= terraform.workspace == "development"
+}
