464bd4141322e857ba28c0b8d99f232458bc116a
Ulises Tirado Zatarain <ulises.tirado@cimat.mx>
2023-07-21 07:35:41 +0100

    LRNT-015: Adding default load balancer
    Signed-off-by: Ulises Tirado Zatarain <ulises.tirado@cimat.mx>

---
+++ alb.tf
@@ -0,0 +1,58 @@
+resource "aws_alb" "entry-point" {
+	for_each = toset(local.configuration.load_balancers)
+  name               = "entry-point" # Naming our load balancer
+  load_balancer_type = "application"
+
+  # Referencing the default subnets
+  subnets = local.subnets.*.id
+  # Referencing the security group
+  security_groups = [
+    aws_security_group.entry-point[each.value].id,
+  ]
+}
+
+# Creating a security group for load balancers
+resource "aws_security_group" "entry-point" {
+	for_each = toset(local.configuration.load_balancers)
+  ingress {
+    from_port   = 80 # Allowing traffic in from port 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] # Allowing traffic in from all sources
+  }
+
+  ingress {
+    from_port   = 443 # Allowing traffic in from port 80
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] # Allowing traffic in from all sources
+  }
+
+  egress {
+    from_port   = 0             # Allowing any incoming port
+    to_port     = 0             # Allowing any outgoing port
+    protocol    = "-1"          # Allowing any outgoing protocol
+    cidr_blocks = ["0.0.0.0/0"] # Allowing traffic out to all IP addresses
+  }
+}
+
+resource "aws_security_group" "alb-access" {
+	for_each = toset(local.configuration.load_balancers)
+  ingress {
+    from_port = 0
+    to_port   = 0
+    protocol  = "-1"
+
+    # Only allowing traffic in from load balancers security group
+    security_groups = [
+      aws_security_group.entry-point[each.value].id,
+    ]
+  }
+
+  egress {
+    from_port   = 0             # Allowing any incoming port
+    to_port     = 0             # Allowing any outgoing port
+    protocol    = "-1"          # Allowing any outgoing protocol
+    cidr_blocks = ["0.0.0.0/0"] # Allowing traffic out to all IP addresses
+  }
+}
