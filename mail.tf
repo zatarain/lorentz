e8c301857836e33c148b04890d14a86e1e864707
Ulises Tirado Zatarain <ulises.tirado@cimat.mx>
2024-08-09 22:34:35 +0100

    LRNT-020: Adding DNS validation records for Mail Service (#33)
    ## 👨🏽‍💻 Description
    
    These changes add the DNS records to validate Mail Service. This will
    allow us to validate the domain names with the CRM Provider via
    authentication from their side.
    
    ## ✅ Testing
    
    - [x] Provisioned the infrastructure to development 
    - [x] Confirm the records were created in Route53
    - [x] Validation runs successful from the CRM Provider
    
    ## 🖼️ Evidence
    
    Following screenshot shows the records created in AWS Route53:
    
    ![image](https://github.com/user-attachments/assets/0f869bc5-64fe-473e-8b22-8093fa3aa01f)
    
    Following screenshot shows the successful authentication from the CRM
    Provider:
    
    ![image](https://github.com/user-attachments/assets/c74edae9-b913-40db-864b-4f39b72d70aa)
    
    ---------
    
    Signed-off-by: Ulises Tirado Zatarain <ulises.tirado@cimat.mx>

--- mail.tf
+++ mail.tf
@@ -10,3 +10,38 @@ resource "aws_secretsmanager_secret_version" "crm-service" {
     dmarc = "change me"
   })
 }
+
+data "aws_secretsmanager_secret_version" "crm-current" {
+  secret_id = aws_secretsmanager_secret.crm-service.id
+}
+
+locals {
+  mail-secret = jsondecode(data.aws_secretsmanager_secret_version.crm-current.secret_string)
+}
+
+resource "aws_route53_record" "crm-code" {
+  for_each = toset(local.configuration.sdlc.environments)
+  zone_id  = local.kingdom.zone_id
+  name     = ""
+  type     = "TXT"
+  ttl      = 172800
+  records  = [local.mail-secret["code"]]
+}
+
+resource "aws_route53_record" "crm-dkim" {
+  for_each = toset(local.configuration.sdlc.environments)
+  zone_id  = local.kingdom.zone_id
+  name     = "mail._domainkey"
+  type     = "TXT"
+  ttl      = 172800
+  records  = [local.mail-secret["dkim"]]
+}
+
+resource "aws_route53_record" "crm-dmarc" {
+  for_each = toset(local.configuration.sdlc.environments)
+  zone_id  = local.kingdom.zone_id
+  name     = "_dmarc"
+  type     = "TXT"
+  ttl      = 172800
+  records  = [local.mail-secret["dmarc"]]
+}
