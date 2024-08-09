f7dc40391bcdf34412cb861ca090e6d94a94cd47
Ulises Tirado Zatarain <ulises.tirado@cimat.mx>
2024-08-09 21:30:14 +0100

    LRNT-020: Adding DNS validation records for Mail Service
    Signed-off-by: Ulises Tirado Zatarain <ulises.tirado@cimat.mx>

--- mail.tf
+++ mail.tf
@@ -10,3 +10,42 @@ resource "aws_secretsmanager_secret_version" "crm-service" {
     dmarc = "change me"
   })
 }
+
+data "aws_secretsmanager_secret_version" "crm-current" {
+  secret_id = aws_secretsmanager_secret.crm-service.id
+}
+
+locals {
+  mail-server = terraform.workspace == "production" ? "" : trimsuffix(local.zone_prefix[terraform.workspace], ".")
+  mail-secret = jsondecode(data.aws_secretsmanager_secret_version.crm-current.secret_string)
+}
+
+resource "aws_route53_record" "crm-code" {
+  for_each = toset(local.records-for-kingdoms)
+  provider = aws.root
+  zone_id  = data.aws_route53_zone.realm[each.value].zone_id
+  name     = local.mail-server == "" ? "@" : local.mail-server
+  type     = "TXT"
+  ttl      = 172800
+  records  = [local.mail-secret["code"]]
+}
+
+resource "aws_route53_record" "crm-dkim" {
+  for_each = toset(local.records-for-kingdoms)
+  provider = aws.root
+  zone_id  = data.aws_route53_zone.realm[each.value].zone_id
+  name     = "mail._domainkey.${local.mail-server}"
+  type     = "TXT"
+  ttl      = 172800
+  records  = [local.mail-secret["dkim"]]
+}
+
+resource "aws_route53_record" "crm-dmarc" {
+  for_each = toset(local.records-for-kingdoms)
+  provider = aws.root
+  zone_id  = data.aws_route53_zone.realm[each.value].zone_id
+  name     = "_dmarc.${local.mail-server}"
+  type     = "TXT"
+  ttl      = 172800
+  records  = [local.mail-secret["dmarc"]]
+}
