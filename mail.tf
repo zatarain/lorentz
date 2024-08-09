1be613927b742f9e91e9ae4cd1dcf77a988211cd
Ulises Tirado Zatarain <ulises.tirado@cimat.mx>
2024-08-09 20:30:28 +0100

    LRNT-020: Adding secrets for mail service authentication
    Signed-off-by: Ulises Tirado Zatarain <ulises.tirado@cimat.mx>

---
+++ mail.tf
@@ -0,0 +1,12 @@
+resource "aws_secretsmanager_secret" "crm-service" {
+  name = "crm-service"
+}
+
+resource "aws_secretsmanager_secret_version" "crm-service" {
+  secret_id     = aws_secretsmanager_secret.crm-service.id
+  secret_string = jsonencode({
+    code = "change me"
+    dkim = "change me"
+    dmarc = "change me"
+  })
+}
